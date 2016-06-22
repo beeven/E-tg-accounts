5b46bfaf5e372409aea40ec3674a29ab8eab4d88
Beeven Yip <beeven@hotmail.com>
2016-06-22 09:25:54 +0800

    Now use observable to process query requests

--- server/company-info-service.js
+++ server/company-info-service.js
@@ -6,13 +6,14 @@ var serviceUrl = config.companyInfoServiceUrl;
 
 exports.getCompanyInfo = function(companyId) {
 
-    /*
+/*
   return Promise.resolve({
     companyId: companyId,
     companyName: "海通临时帐号",
     orgCo: "123456789"
   });
-  */
+*/
+
     return new Promise((resolve,reject)=>{
         request.get(serviceUrl+companyId,function(err,response,body){
             if(err || response.statusCode != 200) {
@@ -31,4 +32,5 @@ exports.getCompanyInfo = function(companyId) {
             // }
         });
     });
+    
 }
--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "database": {
-        url: "mongodb://localhost:27017/test"
+        url: "mongodb://172.7.12.50:27017/test"
     },
     "companyInfoServiceUrl":"http://10.53.1.181:3003/company/getById/"
 }
